a0daa2abb42838eadb224bc6026d8df70ff82c6e
djzib0 <piotrsabatura@gmail.com>
2024-07-01 20:20:40 +0200

    WIP: working on sorting by ingredient type

--- recipe-to-list-app/src/pages/ShopListDetails.tsx
+++ recipe-to-list-app/src/pages/ShopListDetails.tsx
@@ -10,7 +10,8 @@ import { BiArrowBack } from "react-icons/bi";
 import useDatabase from '../customHooks/useDatabase';
 import useModal from '../customHooks/useModal';
 // types import
-import { ShopList, ShopListIngredient } from '../types/types';
+import { Ingredient, ShopList, ShopListIngredient } from '../types/types';
+import { IngredientType } from '../enums/enums';
 
 // array with object for topnavbar
 const topNavbarItems = [
@@ -42,6 +43,8 @@ export default function ShopListDetails() {
 
   // state variables
   const [shopListData, setShopListData] = useState<ShopList>();
+  const [isSortedByType, setIsSortedByType] = useState<boolean>(false);
+  const [shopListIngredients, setShopListIngredients] = useState<Ingredient[]>([]);
 
   useEffect(() => {
     getShopListData(id)
@@ -49,13 +52,27 @@ export default function ShopListDetails() {
 
   useEffect(() => {
     setShopListData(shopListFetchedData?.ingredients && shopListFetchedData)
+    shopListData && setShopListIngredients(shopListData?.ingredients)
   }, [shopListFetchedData])
+
+  // useEffect(() => {
+  //   const ingredientTypes = Object.values(IngredientType)
+  //   const sortedList = shopListIngredients.sort((a: any, b: any) => {
+  //     console.log(console.log(a.ingredientType, "a"))
+  //     console.log(console.log(b, "b"))
+  //     return  b.ingredientType > a.ingredientType
+  //   })
+  //   console.log(sortedList, "sorted list")
+  // }, [isSortedByType])
 
   // functions
   const toggleIsPurchased = (shopListId: string | undefined, ingredientIndex: number, editedObj: ShopListIngredient) => {
     toggleShopListIngredientIsPurchased(shopListId, ingredientIndex, editedObj )
   }
 
+  // if (shopListData?.ingredients && isSortedByType) {
+  //     setShopListData(shopListData?.ingredients.sort((a: any, b: any) => a.ingredientType - b.ingredientType))
+  // }
   // create an array of the ingredients that are not purchased yet
   const notPurchasedShopListIngredientsArr = shopListData && shopListData?.ingredients.filter(item => item.isPurchased === false)
   .map((item, index) => {
@@ -73,7 +90,11 @@ export default function ShopListDetails() {
           />
       </div>
     )
-  }); 
+  });
+
+  const sortByType = () => {
+    setIsSortedByType(true)
+  }
 
   const purchasedShopListIngredientsArr = shopListData?.ingredients.filter(item => item.isPurchased === true)
   .map((item, index) => {
@@ -97,6 +118,7 @@ export default function ShopListDetails() {
     <main>
         <TopNavbar title='details' menuItems={topNavbarItems}/>
       <div className='content__container'>
+        <button onClick={sortByType}>sort by type</button>
         {!shopListFetchedData?.ingredients && <p>No ingredients</p>}
         {notPurchasedShopListIngredientsArr}
         {purchasedShopListIngredientsArr && purchasedShopListIngredientsArr?.length > 0 && <p>Purchased</p>}
